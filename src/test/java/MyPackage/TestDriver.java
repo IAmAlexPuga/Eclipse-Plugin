390736446822eb910452d5f832d67170a640c75a
Alex Puga <alex.puga@wsu.edu>
2020-12-10 16:08:00 -0800

    creaded TestDriver

---
+++ src/test/java/MyPackage/TestDriver.java
@@ -0,0 +1,27 @@
+package MyPackage;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Hashtable;
+
+import org.junit.jupiter.api.Test;
+
+import com.puppycrawl.tools.checkstyle.DefaultConfiguration;
+import com.puppycrawl.tools.checkstyle.DefaultContext;
+import com.puppycrawl.tools.checkstyle.JavaParser;
+import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
+import com.puppycrawl.tools.checkstyle.api.CheckstyleException;
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.FileContents;
+import com.puppycrawl.tools.checkstyle.api.FileText;
+
+public class TestDriver {
+	
+	@Test
+	public void fileTest() {
+		
+	}
+
+}
